91ec4e19d2b593d0d8068c659487086c2033e62e
Derescio <ddw.web.dev.services@gmail.com>
2024-08-19 13:38:19 -0400

    Created Navbar Component

--- client/app/providers/dashboardwrapper.tsx
+++ client/app/providers/dashboardwrapper.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Navbar from '../(components)/Navbar'
 
 const Dashboardwrapper = ({children} : {children: React.ReactNode}) => {
   return (
@@ -6,7 +7,8 @@ const Dashboardwrapper = ({children} : {children: React.ReactNode}) => {
        
         Sidebar
      
-        <main className=' flex flex-col w-full h-full py-7 px-9 bg-gray-200'>
+        <main className= {`flex flex-col w-full h-full py-7 px-9 bg-gray-50 md:pl-[96px] `}>
+            <Navbar />
             {children}
         </main>
        
