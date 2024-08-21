06f232c5ef47eb67a9ff98a2894577d6ea3ebb05
Derescio <ddw.web.dev.services@gmail.com>
2024-08-21 00:11:35 -0400

    Addded Redux state management

--- client/app/(components)/Navbar.tsx
+++ client/app/(components)/Navbar.tsx
@@ -1,26 +1,26 @@
 "use client";
 
-// import { useAppDispatch, useAppSelector } from "@/app/redux";
-// import { setIsDarkMode, setIsSidebarCollapsed } from "@/state";
+import { useAppDispatch, useAppSelector } from "@/app/redux";
+import { setIsDarkMode, setIsSidebarCollapsed } from '@/app/state';
 import { Bell, Menu, Moon, Settings, Sun } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
 const Navbar = () => {
-//   const dispatch = useAppDispatch();
-//   const isSidebarCollapsed = useAppSelector(
-//     (state) => state.global.isSidebarCollapsed
-//   );
-//   const isDarkMode = useAppSelector((state) => state.global.isDarkMode);
+  const dispatch = useAppDispatch();
+  const isSidebarCollapsed = useAppSelector(
+    (state) => state.global.isSidebarCollapsed
+  );
+  const isDarkMode = useAppSelector((state) => state.global.isDarkMode);
 
-//   const toggleSidebar = () => {
-//     dispatch(setIsSidebarCollapsed(!isSidebarCollapsed));
-//   };
+  const toggleSidebar = () => {
+    dispatch(setIsSidebarCollapsed(!isSidebarCollapsed));
+  };
 
-//   const toggleDarkMode = () => {
-//     dispatch(setIsDarkMode(!isDarkMode));
-//   };
+  const toggleDarkMode = () => {
+    dispatch(setIsDarkMode(!isDarkMode));
+  };
 
   return (
     <div className="flex justify-between items-center w-full mb-7">
@@ -28,7 +28,7 @@ const Navbar = () => {
       <div className="flex justify-between items-center gap-5">
         <button
           className="px-3 py-3 bg-gray-100 rounded-full hover:bg-blue-100"
-        //   onClick={toggleSidebar}
+          onClick={toggleSidebar}
         >
           <Menu className="w-4 h-4" />
         </button>
@@ -36,7 +36,7 @@ const Navbar = () => {
         <div className="relative">
           <input
             type="search"
-            placeholder="Start type to search groups & products"
+            placeholder="Search groups/products"
             className="pl-10 pr-4 py-2 w-50 md:w-60 border-2 border-gray-300 bg-white rounded-lg focus:outline-none focus:border-blue-500"
           />
 
@@ -50,13 +50,13 @@ const Navbar = () => {
       <div className="flex justify-between items-center gap-5">
         <div className="hidden md:flex justify-between items-center gap-5">
           <div>
-            {/* <button onClick={toggleDarkMode}>
+            <button onClick={toggleDarkMode}>
               {isDarkMode ? (
                 <Sun className="cursor-pointer text-gray-500" size={24} />
               ) : (
                 <Moon className="cursor-pointer text-gray-500" size={24} />
               )}
-            </button> */}
+            </button>
           </div>
           <div className="relative">
             <Bell className="cursor-pointer text-gray-500" size={24} />
@@ -80,6 +80,7 @@ const Navbar = () => {
           <Settings className="cursor-pointer text-gray-500" size={24} />
         </Link>
       </div>
+
     </div>
   );
 };
--- client/app/providers/dashboardwrapper.tsx
+++ client/app/providers/dashboardwrapper.tsx
@@ -1,20 +1,48 @@
-import React from 'react'
+"use client";
+
+import React, { useEffect } from "react";
 import Navbar from '../(components)/Navbar'
+import SideBar from '../(components)/SideBar'
+import StoreProvider, { useAppSelector } from '@/app/redux';
 
-const Dashboardwrapper = ({children} : {children: React.ReactNode}) => {
-  return (
-    <div className= {`light flex bg-gray-50 text-gray-900  min-h-screen`}>
-       
-        Sidebar
-     
-        <main className= {`flex flex-col w-full h-full py-7 px-9 bg-gray-50 md:pl-[96px] `}>
-            <Navbar />
-            {children}
-        </main>
-       
+const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+    const isSidebarCollapsed = useAppSelector(
+      (state) => state.global.isSidebarCollapsed
+    );
+    const isDarkMode = useAppSelector((state) => state.global.isDarkMode);
   
-    </div>
-  )
-}
+    useEffect(() => {
+      if (isDarkMode) {
+        document.documentElement.classList.add("dark");
+      } else {
+        document.documentElement.classList.add("light");
+      }
+    });
+  
+    return (
+      <div
+        className={`${
+          isDarkMode ? "dark" : "light"
+        } flex bg-gray-50 text-gray-900 w-full min-h-screen`}
+      >
+        <SideBar />
+        <main
+  className={`flex flex-col w-full h-full py-7 px-9 bg-gray-50 ${isSidebarCollapsed ? "md:pl-24" : "md:pl-72"}`}
+>
 
-export default Dashboardwrapper
+          <Navbar />
+          {children}
+        </main>
+      </div>
+    );
+  };
+  
+  const DashboardWrapper = ({ children }: { children: React.ReactNode }) => {
+    return (
+      <StoreProvider>
+        <DashboardLayout>{children}</DashboardLayout>
+      </StoreProvider>
+    );
+  };
+  
+  export default DashboardWrapper;
